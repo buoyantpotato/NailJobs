d039f98b1f0f71e514c59f2b1545b9860b0b6326
buoyantpotato <buoyantpotato@gmail.com>
2017-03-06 15:10:43 -0800

    Test

---
+++ P001_TwoSum.java
@@ -0,0 +1,8 @@
+/**
+ * Created by linzhou on 3/6/17.
+ */
+public class P001_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[2];
+    }
+}
---
+++ TreeNode.java
@@ -0,0 +1,12 @@
+/**
+ * Created by linzhou on 2016/11/29.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int v) {
+        val = v;
+    }
+}
